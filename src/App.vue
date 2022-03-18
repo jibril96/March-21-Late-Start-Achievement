cf2ca2daccbd1d43a76000002dcba657261775e2
jibril96 <jibbs_nba@hotmail.com>
2022-03-17 21:08:28 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+    <TheHeader :title = "pageHeader"  :description = "pageDescription" />
+    <main>
+      <p>This is on the home page</p>
+      </main>
+      <TheFooter />
+  </div>
+</template>
+
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+const pageHeader = 'HomePage!';
+const pageDescription = 'This is the landing page';
+</script>
+
+<style>
+ #pp {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
